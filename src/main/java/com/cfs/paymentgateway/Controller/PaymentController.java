ab99a5a492852c52de61ffd4bb714f94f6054c73
Aman Gautam <amangautam2128@gmail.com>
2025-10-08 16:50:09 +0530

    Created Controller

--- src/main/java/com/cfs/paymentgateway/Controller/PaymentController.java
+++ src/main/java/com/cfs/paymentgateway/Controller/PaymentController.java
@@ -1,11 +1,33 @@
 package com.cfs.paymentgateway.Controller;
 
 
+import com.cfs.paymentgateway.Service.PaymentService;
+import com.cfs.paymentgateway.entity.PaymentOrder;
+import org.springframework.beans.factory.annotation.Autowired;
+import org.springframework.boot.autoconfigure.AutoConfigureOrder;
+import org.springframework.http.HttpStatus;
+import org.springframework.http.ResponseEntity;
+import org.springframework.web.bind.annotation.PostMapping;
+import org.springframework.web.bind.annotation.RequestBody;
 import org.springframework.web.bind.annotation.RequestMapping;
 import org.springframework.web.bind.annotation.RestController;
 
 @RestController
-@RequestMapping("/payment/createOrder")
+@RequestMapping("api/payment")
 public class PaymentController {
+
+    @Autowired
+    private PaymentService paymentService;
+
+    @PostMapping("/create-order")
+    public ResponseEntity<String> createOrder(@RequestBody PaymentOrder order) {
+        try {
+            String response = paymentService.createOrder(order);
+            return ResponseEntity.ok(response);
+        }
+        catch (Exception e) {
+            return ResponseEntity.status(HttpStatus.INTERNAL_SERVER_ERROR).body("Error Creating Order");
+        }
+    }
 
 }
